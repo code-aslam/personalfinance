b0cda9ba3b22ac6749c2f5f2d90104c2621ab066
NAGARRO\aslamkhan <project.aslam@gmail.com>
2025-02-04 14:18:38 +0530

    Implement splash/login screen and firebase google login

--- app/src/main/java/com/hotdogcode/spendwise/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/hotdogcode/spendwise/presentation/home/HomeActivity.kt
@@ -1,17 +1,23 @@
-package com.hotdogcode.spendwise.presentation
+package com.hotdogcode.spendwise.presentation.home
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.hotdogcode.spendwise.navigation.AppNavigation
-import com.hotdogcode.spendwise.ui.theme.PersonalFinanceTheme
+import com.hotdogcode.spendwise.presentation.home.ui.theme.PersonalFinanceTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -53,17 +59,3 @@ fun SetupStatusBar(){
 //        onDispose { }
 //    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
